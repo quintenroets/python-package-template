0c1dfa7529c37d336d036db3dcfaed1a7076c5ee
Quinten <quinten.roets@gmail.com>
2023-12-08 22:40:20 -0800

    don't bump in github actions

--- scripts/pre-commit/bump_version.sh
+++ scripts/pre-commit/bump_version.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-if ! git diff --staged pyproject.toml | grep -q version; then
-    bump2version --config-file .bumpversion.cfg patch
+# don't bump/check bump in GitHub actions
+if [ "$GITHUB_ACTIONS" != "true" ]; then
+    if ! git diff --staged pyproject.toml | grep -q version; then
+        bump2version --config-file .bumpversion.cfg patch
+    fi
 fi
