a8756520615040c7866d4fc0fa3653964a3ca700
Quinten <quinten.roets@gmail.com>
2024-02-04 12:48:04 -0800

    run mypy hook in dev environment

--- src/python_package_template/models/path.py
+++ src/python_package_template/models/path.py
@@ -4,15 +4,15 @@ import plib
 from simple_classproperty import classproperty
 
 
-class Path(plib.Path):  # type: ignore
+class Path(plib.Path):
     @classproperty
-    def source_root(cls: type[Path]) -> Path:
+    def source_root(cls) -> Path:
         return cls(__file__).parent.parent  # type: ignore
 
     @classproperty
-    def assets(cls: type[Path]) -> Path:
+    def assets(cls) -> Path:
         return cls.script_assets / cls.source_root.name  # type: ignore
 
     @classproperty
-    def config(cls: type[Path]) -> Path:
+    def config(cls) -> Path:
         return cls.assets / "config" / "config.yaml"  # type: ignore
