a24d34470db6860ea3470ae52fa2b4770b4c8af0
Quinten Roets <62651391+quintenroets@users.noreply.github.com>
2024-06-29 19:16:11 -0700

    use ruff best practices (#27)

--- src/python_package_template/cli/entry_point.py
+++ src/python_package_template/cli/entry_point.py
@@ -1,7 +1,6 @@
 from package_utils.context.entry_point import create_entry_point
 
 from python_package_template import main
-
-from ..context import context
+from python_package_template.context import context
 
 entry_point = create_entry_point(main, context)
